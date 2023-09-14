174c6e012fbc0cf672147899e2e05701cbde679a
kim <gayun53a@naver.com>
2023-09-14 09:49:55 +0900

    lambda function and related packages like requests and selenium for lambda

---
+++ lambda_function.py
@@ -0,0 +1,106 @@
+import json
+import boto3
+import requests
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('disasterMsgDB')  # DynamoDB 테이블 이름으로 바꾸십시오.
+
+
+def checkDB(md101_sn):
+    existing_item = table.get_item(Key={'md101_sn': md101_sn})
+    return 'Item' in existing_item
+
+
+def getbasicInfo():
+    response = requests.get(
+        'http://apis.data.go.kr/1741000/DisasterMsg3/getDisasterMsg1List?ServiceKey=[YOUR SERVICE KEY]&type=json&pageNo=1&numOfRows=1')
+    response.raise_for_status()
+    data = response.json()
+    item = data['DisasterMsg'][1]['row'][0]
+    return item
+
+
+def getAdditionalinfo():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1280x1696')
+    chrome_options.add_argument('--user-data-dir=/tmp/user-data')
+    chrome_options.add_argument('--hide-scrollbars')
+    chrome_options.add_argument('--enable-logging')
+    chrome_options.add_argument('--log-level=0')
+    chrome_options.add_argument('--v=99')
+    chrome_options.add_argument('--single-process')
+    chrome_options.add_argument('--data-path=/tmp/data-path')
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--homedir=/tmp')
+    chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
+    chrome_options.add_argument(
+        'user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
+    chrome_options.binary_location = "/opt/python/bin/headless-chromium"
+
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path='/opt/python/bin/chromedriver')
+    URL = "https://www.safekorea.go.kr/idsiSFK/neo/sfk/cs/sfc/dis/disasterMsgList.jsp?menuSeq=679"
+    driver.get(URL)
+
+    data_dict = {}
+
+    try:
+        # 페이지의 첫 번째 요소가 로드될 때까지 최대 10초간 대기합니다.
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "disasterSms_tr_0_apiData1"))
+        )
+
+        disaster_type = driver.find_element(By.ID, f"disasterSms_tr_0_DSSTR_SE_NM").text
+        emergency_step = driver.find_element(By.ID, f"disasterSms_tr_0_EMRGNCY_STEP_NM").text
+
+        data_dict = {
+            "disaster_type": disaster_type,
+            "emergency_step": emergency_step,
+        }
+
+    finally:
+        driver.quit()
+
+    return data_dict
+
+
+def lambda_handler(event, context):
+    try:
+        item = getbasicInfo()
+
+        md101_sn = item.get('md101_sn')
+        if not checkDB(md101_sn):
+            additional_info = getAdditionalinfo()
+            item.update(additional_info)
+            current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            item['stored_time'] = current_time
+            table.put_item(Item=item)
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Process Complete!')
+        }
+
+    except requests.RequestException as re:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Failed to fetch data from API. Error: {str(re)}")
+        }
+    except ValueError as ve:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Data error: {str(ve)}")
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Unknown error occurred: {str(e)}")
+        }
